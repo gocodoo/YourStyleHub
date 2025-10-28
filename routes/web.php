d1875e45fe03c9f075a64cfb12363f9085469b21
Vanna Sreyne <vannasreyne3@gmail.com>
2025-10-28 23:05:12 +0700

    frontend yourstyle

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\YourStyleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('filament.admin.auth.login');
 });
+
+Route::get('/yourstyle', [YourStyleController::class, 'index'])->name('yourstyle');
